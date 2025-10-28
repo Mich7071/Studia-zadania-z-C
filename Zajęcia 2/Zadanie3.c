40e0e5c7d3a35be9a580cf985bd474137ee85646
Michał Michalczuk <michalmichalczuk01@gmail.com>
2025-10-28 20:47:22 +0100

    Add files via upload

--- Zajęcia 2/Zadanie3.c
+++ Zajęcia 2/Zadanie3.c
@@ -1,24 +1,114 @@
-//Napisz program, który monitoruje wyniki pomiarów z czujnika temperatury. Program wczytuje kolejne wartości całkowite reprezentujące odczyty z czujnika, dopóki ta sama wartość nie pojawi się trzy razy z rzędu. Taki przypadek oznacza, że temperatura ustabilizowała się i pomiary można zakończyć. Po wykryciu trzykrotnego powtórzenia program kończy działanie, wyświetlając komunikat informujący o stabilnym odczycie.
+/*Zadanie główne - System Sterowania Statkiem „Orion One”
+Napisz program symulujący terminal sterowania energią statku kosmicznego „Orion One”. System pozwala kapitanowi zarządzać poziomem energii rdzenia napędu, przechowywanym w jednej zmiennej.
+Po uruchomieniu systemu należy wprowadzić liczbowy kod kapitana. Tylko jeden kod jest poprawny.
+Po błędnym kodzie wyświetlany jest komunikat o nieudanej próbie logowania wraz z informacją, ile prób (z trzech możliwych) jeszcze pozostało.
+Po trzech nieudanych próbach wyświetlany jest odpowiedni komunikat i program kończy działanie.
+Po poprawnym logowaniu system wyświetla menu:
+1 – Charge the reactor core: wczytaj dodatnią wartość i dodaj ją do poziomu energii.
+2 – Discharge the reactor core: wczytaj wartość i odejmij od poziomu energii (jeśli wystarczająca).
+3 – Check energy level: wyświetl aktualny poziom energii.
+0 – Log out: powrót do logowania (poziom energii jest zapamiętywany przez cały czas działania programu).
+Jeśli użytkownik wybierze niedostępną opcję (np. 4), program powinien o tym poinformować i ponownie wyświetlić menu.
+Każdorazowo system wyświetla informację, czy operacja została wykonana prawidłowo czy też nie (ujemna wartość energii, lub zbyt duża wartość zużycia)
+Dodatkowo system jest zabezpieczony przed podaniem wartości energii innej niż liczbowa - w takiej sytuacji wyświetla komunikat i ponawia wczytanie wartości (do skutku).*/
 
 #include <stdio.h>
 
+int moc=100;
+
 int main() {
-    int x=0,y=0,z=0,zapisy=0;
+    while (1) {
+        int i=0,haslo=1234,proba=0;//dane Logowanie
+        int l=1;//menu
 
-    //Odczytywanie i porównywanie wyników
-     while (x != y || y != z || zapisy<3){
+        //Logowanie
+        while (1) {
+            i+=1;
+            printf("Podaj Haslo: ");
 
-        z=y;
-        y=x;
+            //Sprawdzanie poprawności danych
+            scanf("%d",&proba);
+            if (getchar()!='\n') {
+                while (getchar() != '\n');
+                printf("\nZle haslo, pozostalo prob %d\n",3-i);
+                if(i==3) {
+                    printf("\nKoniec prob");
+                    return 0;
+                }
+                continue;
+            }
 
-        if (scanf("%d",&x) !=1) {
-            printf("Zle dane!");
-            return 1;
+
+            //Sprawdzanie
+            if (proba==haslo) {
+                printf("\nHaslo Poprawne.\n");
+                break;
+            }else if(i==3) {
+                printf("\nKoniec prob");
+                return 0;
+            }else {
+                printf("\nZle haslo, pozostalo prob %d\n",3-i);
+            }
         }
-         zapisy+=1;
+
+        //--------------------------------------------------------------------------------------
+        //                                     Menu
+        //--------------------------------------------------------------------------------------
+
+        int x,wartosc;
+
+        do {
+            printf("\n1 - Charge the reactor core:\n"
+                   "2 - Discharge the reactor core:\n"
+                   "3 - Check energy level:\n"
+                   "0 - Log out:\n");
+
+            //wybor
+            if (scanf("%d",&x) !=1) {
+                printf("\n Nie ma takiej opcji\n");
+                while (getchar() != '\n');
+                continue;
+            }
+
+            switch (x) {
+
+                case 1:
+                    printf("Podaj wartosc mocy jaka dodac: ");
+
+                    if (scanf("%d",&wartosc) !=1 || wartosc<0) {
+                        printf("\n wartosc nie prawidlowa\n");
+                        while (getchar() != '\n');
+                        break;
+                    }
+                    moc+=wartosc;
+                    printf("\nOperacja udana\n");
+                    break;
+
+                case 2:
+                    printf("Podaj wartosc mocy jaka odjac: ");
+
+                    if (scanf("%d",&wartosc) !=1 ||moc<wartosc || wartosc<0) {
+                        printf("\n zbyt duża wartość zużycia lub zle dane!\n");
+                        while (getchar() != '\n');
+                        break;
+                    }
+                    moc-=wartosc;
+                    printf("\nOperacja udana\n");
+                    break;
+
+                case 3:
+                    printf("Aktualna wartosc energi to: %d\n",moc);
+                    break;
+
+                case 0:
+                    l=0;
+                    break;
+
+                default:
+                    printf("\n Nie ma takiej opcji\n");
+                    break;
+            }
+
+        }while (l==1);
     }
-
-    printf("Stabilny odczyt");
-
-    return 0;
 }
