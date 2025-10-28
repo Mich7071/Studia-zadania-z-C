2423503746fc00918492d36cdebabc318be247d5
Michał Michalczuk <michalmichalczuk01@gmail.com>
2025-10-28 17:09:34 +0100

    Add files via upload

---
+++ Zajęcia 2/Zadanie3.c
@@ -0,0 +1,24 @@
+//Napisz program, który monitoruje wyniki pomiarów z czujnika temperatury. Program wczytuje kolejne wartości całkowite reprezentujące odczyty z czujnika, dopóki ta sama wartość nie pojawi się trzy razy z rzędu. Taki przypadek oznacza, że temperatura ustabilizowała się i pomiary można zakończyć. Po wykryciu trzykrotnego powtórzenia program kończy działanie, wyświetlając komunikat informujący o stabilnym odczycie.
+
+#include <stdio.h>
+
+int main() {
+    int x=0,y=0,z=0,zapisy=0;
+
+    //Odczytywanie i porównywanie wyników
+     while (x != y || y != z || zapisy<3){
+
+        z=y;
+        y=x;
+
+        if (scanf("%d",&x) !=1) {
+            printf("Zle dane!");
+            return 1;
+        }
+         zapisy+=1;
+    }
+
+    printf("Stabilny odczyt");
+
+    return 0;
+}
