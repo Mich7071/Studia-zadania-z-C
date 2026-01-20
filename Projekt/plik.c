b736bb57cb3f5ade604e9ec396f357d9319561f2
Michał Michalczuk <michalmichalczuk01@gmail.com>
2026-01-20 18:30:33 +0100

    główna zmiana

--- Projekt/plik.c
+++ Projekt/plik.c
@@ -11,15 +11,15 @@ struct Bohaterowie *lista() {
         printf("Error pobrania pliku\n");
         exit(1);
     }
-    struct Bohaterowie *glowa = NULL; // Na początku lista jest pusta
+    struct Bohaterowie *HEAD = NULL;
     char buforImie[102];
 
     while (fscanf(f, "%101s", buforImie) == 1) {
 
-        // 1. Tworzymy nowy element
+
         struct Bohaterowie *nowy = malloc(sizeof(struct Bohaterowie));
 
-        // 2. Wypełniamy danymi
+
         strcpy(nowy->imie, buforImie);
         fscanf(f, "%14s", nowy->rasa);
         fscanf(f, "%d", &nowy->poziom);
@@ -31,11 +31,11 @@ struct Bohaterowie *lista() {
         nowy->status = (StatusBohatera)status;
         fscanf(f, "%d", &nowy->dni);
 
-        // 3. MAGICZNY MOMENT (Wstawianie na początek listy jednokierunkowej)
-        nowy->Next = glowa; // Nowy trzyma za rękę "starą" głowę
-        glowa = nowy;           // Nowy staje się "nową" głową
+
+        nowy->Next = HEAD;
+        HEAD = nowy;
     }
 
     fclose(f);
-    return glowa;
+    return HEAD;
 }
